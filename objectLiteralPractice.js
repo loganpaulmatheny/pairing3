a7afeb029f49d8ee5d07233b7f2ec23630d8e6ce
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-24 16:29:25 -0400

    Add object literal practice

---
+++ objectLiteralPractice.js
@@ -0,0 +1,9 @@
+var person = { name: "Logan", age: 30, height: 60, eyes: "Hazel" };
+
+console.log(person);
+
+person.sport = "soccer";
+person.bulletJournal = true;
+
+console.log(person);
+console.log(Object.keys(person));
